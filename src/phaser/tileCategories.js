6ec589c89fc65dd1c01a89ee81ef223e48f7df05
LifeHckr <jarodthereal@gmail.com>
2024-12-10 17:35:21 -0800

    Created basic tile categories
    Could be tweaked for what we need.
    Comments have sub category hints if we need more specificity

---
+++ src/phaser/tileCategories.js
@@ -0,0 +1,135 @@
+const tilecategories = {
+    1: "ground",
+    2: "ground",
+    3: "ground",
+    4: "plantlife", //tree
+    5: "plantlife", //tree
+    6: "plantlife", //shrub
+    7: "plantlife", //shrub
+    8: "plantlife", //shrub
+    9: "plantlife", //shrub
+    10: "plantlife", //shrub
+    11: "plantlife", //shrub
+    12: "plantlife", //shrub
+    13: "path",
+    14: "path",
+    15: "path",
+    16: "plantlife", //tree
+    17: "plantlife", //tree
+    18: "plantlife", //plant
+    19: "plantlife", //shrub
+    20: "plantlife", // shrub
+    21: "plantlife", //shrub
+    22: "plantlife", //shrub
+    23: "plantlife", //shrub
+    24: "plantlife", //shrub
+    25: "path",
+    26: "path",
+    27: "path",
+    28: "plantlife", //shrub
+    29: "plantlife", //shrub
+    30: "plantlife", //plant
+    31: "plantlife", //shrub
+    32: "plantlife", //shrub
+    33: "plantlife", //shrub
+    34: "plantlife", //shrub
+    35: "plantlife", //shrub
+    36: "plantlife", //shrub
+    37: "path",
+    38: "path",
+    39: "path",
+    40: "path",
+    41: "path",
+    42: "path",
+    43: "path",
+    44: "path",
+    45: "fence",
+    46: "fence",
+    47: "fence",
+    48: "fence",
+    49: "house roof", //grey roof
+    50: "house roof", //grey roof
+    51: "house roof", //grey roof
+    52: "house roof", //grey roof
+    53: "house roof", //red roof
+    54: "house roof", //red roof
+    55: "house roof", //red roof
+    56: "house roof", //red roof
+    57: "fence",
+    58: "object",
+    59: "fence",
+    60: "fence",
+    61: "house roof", //grey roof
+    62: "house roof", //grey roof
+    63: "house roof", //grey roof
+    64: "house roof", //grey roof
+    65: "house roof", //red roof
+    66: "house roof", //red roof
+    67: "house roof", //red roof
+    68: "house roof", //red roof
+    69: "fence",
+    70: "fence",
+    71: "fence",
+    72: "fence",
+    73: "house", //brown house
+    74: "house", //brown house
+    75: "house", //brown house
+    76: "house", //brown house
+    77: "house", //grey house
+    78: "house", //grey house
+    79: "house", //grey house
+    80: "house", //grey house
+    81: "fence",
+    82: "fence",
+    83: "fence",
+    84: "object",
+    85: "house", //brown house
+    86: "house", //brown house
+    87: "house", //brown house
+    88: "house", //brown house
+    89: "house", //grey house
+    90: "house", //grey house
+    91: "house", //grey house
+    92: "house", //grey house
+    93: "object",
+    94: "object",
+    95: "object",
+    96: "object",
+    97: "castle roof",
+    98: "castle roof",
+    99: "castle roof",
+    100: "castle roof",
+    101: "castle roof",
+    102: "castle roof",
+    103: "castle roof",
+    104: "castle roof",
+    105: "object",
+    106: "object",
+    107: "object",
+    108: "object",
+    109: "castle roof",
+    110: "castle roof",
+    111: "castle roof",
+    112: "castle wall",
+    113: "castle wall",
+    114: "castle wall",
+    115: "castle wall",
+    116: "object",
+    117: "object",
+    118: "object",
+    119: "object",
+    120: "object",
+    121: "castle roof",
+    122: "castle roof",
+    123: "castle roof",
+    124: "castle wall",
+    125: "castle wall",
+    126: "castle wall",
+    127: "castle wall",
+    128: "object",
+    129: "object",
+    130: "object",
+    131: "object",
+    132: "object"
+}
+export {tilecategories};
